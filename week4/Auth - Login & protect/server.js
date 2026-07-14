801a1a48706659a2e29f40e89204ef092803484e
AREVETI SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2026-07-14 18:14:28 +0530

    Create  server.js

---
+++ week4/Auth - Login & protect/server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+require("dotenv").config();
+
+const app=express();
+
+app.use(express.json());
+
+app.use("/api/auth",require("./routes/authRoutes"));
+
+app.listen(process.env.PORT,()=>{
+
+    console.log("Server Running");
+
+});
